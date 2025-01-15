4d1848c7feab267c086936187d91410a324a6e6a
David Nazare <dsnazare@icloud.com>
2025-01-14 21:34:27 -0300

    Retirando import desnecessario

--- src/main/java/com/fiap/parquimetro/core/domain/Ticket.java
+++ src/main/java/com/fiap/parquimetro/core/domain/Ticket.java
@@ -3,8 +3,6 @@ package com.fiap.parquimetro.core.domain;
 import java.util.Date;
 import java.util.UUID;
 
-import com.fiap.parquimetro.core.domain.exceptions.DomainStateException;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
